ff89432da87d1c0aa20a942daac8e99275ceade7
Mohammed Patel <mopatel1214@gmail.com>
2020-05-28 16:15:49 -0500

    final changes for 5/28

--- src/aboutC/aboutC.jsx
+++ src/aboutC/aboutC.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 //import ReactDOM from 'react-dom';
-//import './about.css';
+import '../about.css';
 
 
 
@@ -11,13 +11,13 @@ export class About extends React.Component {
         <h2 style= {{textAlign: "center",textIndent: "40px", fontSize: '32px', color: '#434343', fontFamily: 'Open Sans Condensed'}}>Club Executives</h2>
         <p style= {{textAlign: 'center', fontSize: '24px', color: '#434343', fontFamily: 'Open Sans Condensed'}}>Click on the names to learn more about each executive and their current roles/tasks</p>
         
-        <ol>
+        <ul>
           <li style= {{textAlign: 'center', fontSize: '24px', color: '#434343', fontFamily: 'Open Sans Condensed', }} >Founder & President: Adam Patel</li>
-          <li className = "aboutStuff" style= {{textAlign: 'center', fontSize: '24px', color: '#434343', fontFamily: 'Open Sans Condensed'}}>Vice President: Jaser Siddiqui</li>
-          <li className = "aboutStuff" style= {{textAlign: 'center', fontSize: '24px', color: '#434343', fontFamily: 'Open Sans Condensed'}}>Treasurer: Gerardo Garcia</li>
-          <li className = "aboutStuff" style= {{textAlign: 'center', fontSize: '24px', color: '#434343', fontFamily: 'Open Sans Condensed'}}>Lead Website Developer: Mohammed Patel</li>
-          <li className = "aboutStuff" style= {{textAlign: 'center', fontSize: '24px', color: '#434343', fontFamily: 'Open Sans Condensed'}}>Secretary: Mohammad Larya</li>
-        </ol>
+          <li style= {{textAlign: 'center', fontSize: '24px', color: '#434343', fontFamily: 'Open Sans Condensed'}}>Vice President: Jaser Siddiqui</li>
+          <li style= {{textAlign: 'center', fontSize: '24px', color: '#434343', fontFamily: 'Open Sans Condensed'}}>Treasurer: Gerardo Garcia</li>
+          <li style= {{textAlign: 'center', fontSize: '24px', color: '#434343', fontFamily: 'Open Sans Condensed'}}>Lead Website Developer: Mohammed Patel</li>
+          <li style= {{textAlign: 'center', fontSize: '24px', color: '#434343', fontFamily: 'Open Sans Condensed'}}>Secretary: Mohammad Larya</li>
+        </ul>
 
         <p style= {{textAlign: 'center', fontSize: '24px', color: '#434343', fontFamily: 'Open Sans Condensed'}}>Interested in pursuing an executive role? Email <strong>apatel56@luc.edu</strong> to get the ball rolling and see where you'll fit in best!</p>
       </div>
